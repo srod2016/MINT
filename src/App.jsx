43e805b3e7e552f7385bb6fe0881f76222945a22
Javier Gil <javigq@gmail.com>
2025-11-28 21:51:49 -0500

    Added initial navigation frameworks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
 import './App.css'
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Layout from "./modules/Layout";
+import Dashboard from "./pages/Dashboard";
+import Transactions from "./pages/Transactions";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Settings";
 
 function App() {
 
@@ -9,6 +20,18 @@ function App() {
       <h1 className="text-3xl font-bold">MINT Budget Tracker</h1>
     </div>
     
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* index = default child at "/" */}
+          <Route index element={<Dashboard />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="reports" element={<Reports />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+
     </>
   )
 }
