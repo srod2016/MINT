eea7f1ca4d4ef5cfeb1239549c2a707a5fb85152
antmedmia@gmail.com <antmedmia@gmail.com>
2025-11-28 23:17:15 -0500

    commit vite config - force vite dependency

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    // tailwindcss(),  <-- Commented out
   ],
+  server: {
+    // Server config issues so I'm forcing IPv4 and a specific port
+    host: '127.0.0.1', // Forces IPv4 
+    port: 3000,        // Forced port
+    strictPort: true,
+  }
 })
